47449d4d69d145b970b8648148f0c5705f50bef6
David Duong <david@duong.cz>
2015-02-16 19:22:44 +0100

    Fixing error fallback

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,26 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
 var livereload = require("gulp-livereload");
+var plumber = require('gulp-plumber');
+var notify = require("gulp-notify");
+
 
 gulp.task("styles", function() {
-	return gulp.src("./assets/styles/*.less").pipe(less()).pipe(gulp.dest("./assets/styles")).pipe(livereload());
+	return gulp.src("./assets/styles/*.less").pipe(plumber(notify.onError("Error: <%= error.message %>"))).pipe(less()).pipe(gulp.dest("./assets/styles")).pipe(livereload());
+
 });
 
 gulp.task("scripts", function() {
-	return gulp.src("./assets/scripts/**").pipe(livereload());
+	return gulp.src("./assets/scripts/**").pipe(plumber(notify.onError("Error: <%= error.message %>"))).pipe(livereload());
 });
 
 gulp.task("html", function() {
-	return gulp.src("./*.html").pipe(livereload());
+	return gulp.src("./*.html").pipe(plumber(notify.onError("Error: <%= error.message %>"))).pipe(livereload());
 });
 
 gulp.task("watch", function() {
 	livereload.listen();
-	gulp.watch("./assets/styles/**", ["styles"]);
+	gulp.watch("./assets/styles/*.less", ["styles"]);
 	gulp.watch("./assets/scripts/**", ["scripts"]);
 	gulp.watch("./*.html", ["html"]);
 });
